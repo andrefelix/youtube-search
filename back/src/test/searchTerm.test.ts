2ad267874f0b47a2aa34dc005569feb2dd2690e4
andrefelix <andre.felix@amedigital.com>
2021-02-16 03:13:15 -0300

    feat: adicionado teste para assegurar que o valor de minutesAvailableWeek seja um Array convertido para JSON

--- back/src/test/searchTerm.test.ts
+++ back/src/test/searchTerm.test.ts
@@ -1,7 +1,7 @@
 import { Request } from 'express';
 import { SearchTerm } from '../controllers/searchTerm';
 import { ErrorResponseInterface } from '../interfaces/errorResponseInterface';
-import { missingParamError } from '../helpers/paramError';
+import { missingParamError, invalidParamError } from '../helpers/paramError';
 
 const makeResponse: any = () => {
   const res = { status: jest.fn(), send: jest.fn(), json: jest.fn() };
@@ -62,6 +62,20 @@ describe('SearchTerm', () => {
     expect(mockResponse.json).toBeCalledTimes(1);
     expect(mockResponse.json).toBeCalledWith(
       makeErrorResponse(missingParamError('minutesAvailableWeek'), 400)
+    );
+  });
+
+  it('deveria retornar statusCode 400 com a mensagem de que a query minutesAvailableWeek é inválida', () => {
+    const mockResponse = makeResponse();
+    const mockRequest = makeRequest();
+
+    mockRequest.query = { term: 'term', minutesAvailableWeek: 'invalid-value' };
+
+    searchTerm.get(mockRequest, mockResponse);
+    expect(mockResponse.status).toHaveBeenNthCalledWith(1, 400);
+    expect(mockResponse.json).toBeCalledTimes(1);
+    expect(mockResponse.json).toBeCalledWith(
+      makeErrorResponse(invalidParamError('minutesAvailableWeek'), 400)
     );
   });
 });
