e642d279cb9b634c112870431900f657831c5062
andrefelix <andre.felix@amedigital.com>
2021-02-16 02:05:55 -0300

    feat: adicionado verificação de parâmetro ao acessar endpoint

--- back/src/controllers/searchTerm.ts
+++ back/src/controllers/searchTerm.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import { HttpResponse } from '../helpers/httpResponse';
+import { missingParamError } from '../helpers/paramError';
 
 interface SearchTermInterface {
   get(resquest: Request, response: Response): Response;
@@ -6,7 +8,18 @@ interface SearchTermInterface {
 
 const SearchTerm = (): SearchTermInterface => {
   const get = (request: Request, response: Response) => {
-    return response.status(200).send();
+    const httpResponse = HttpResponse(response);
+    const { term } = request.query;
+
+    try {
+      if (!term) {
+        return httpResponse.badRequest(missingParamError('term'));
+      }
+
+      return httpResponse.ok();
+    } catch (err) {
+      return httpResponse.serverError();
+    }
   };
 
   return { get };
