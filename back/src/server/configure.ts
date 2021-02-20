1fea85d6ebbfa0d0b7507601d34f077173473bbc
andrefelix <andre.felix@amedigital.com>
2021-02-20 04:18:46 -0300

    feat: adicionado configuração de cors e logs para ambiente dev

--- back/src/server/configure.ts
+++ back/src/server/configure.ts
@@ -1,4 +1,5 @@
-import express, { Application } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import morgan from 'morgan';
 import { config } from 'dotenv';
 
 config();
@@ -6,6 +7,13 @@ config();
 import { Routes } from './routes';
 
 const Configure = (app: Application): Application => {
+  app.use((request: Request, response: Response, next: NextFunction) => {
+    response.header('Access-Control-Allow-Origin', '*');
+    response.header('Access-Control-Allow-Methods', 'GET');
+    next();
+  });
+
+  app.use(morgan('dev'));
   app.use(express.json());
 
   Routes(app);
